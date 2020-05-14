6fca05b617b84688302ca8f8026c0388d7df1863
Seung Jin Cho <whtmdwls20@naver.com>
2020-05-14 23:51:20 +0900

    Completed Exercise 4 in Assignment 7.

---
+++ exercise_4.R
@@ -0,0 +1,55 @@
+# Exercise 4: practicing with dplyr
+
+# Install the `"nycflights13"` package. Load (`library()`) the package.
+# You'll also need to load `dplyr`
+install.packages("nycflights13")
+library(nycflights13)
+library("dplyr")
+print(flights)
+library(ggplot2)
+
+# The data frame `flights` should now be accessible to you.
+# Use functions to inspect it: how many rows and columns does it have?
+# What are the names of the columns?
+# Use `??flights` to search for documentation on the data set (for what the 
+# columns represent)
+nrow(flights)
+ncol(flights)
+colnames(flights)
+
+
+# Use `dplyr` to give the data frame a new column that is the amount of time
+# gained or lost while flying (that is: how much of the delay arriving occured
+# during flight, as opposed to before departing).
+flights <- flights %>% 
+  mutate(
+    gain = dep_delay - arr_delay,
+    hours = air_time/60,
+    gain_in_air = gain/hours
+  )
+
+# Use `dplyr` to sort your data frame in descending order by the column you just
+# created. Remember to save this as a variable (or in the same one!)
+flights_descending <- arrange(flights, -dep_delay, arr_delay)
+print(flights_descending)
+
+# For practice, repeat the last 2 steps in a single statement using the pipe
+# operator. You can clear your environmental variables to "reset" the data frame
+
+
+# Make a histogram of the amount of time gained using the `hist()` function
+hist(flights$gain_in_air)
+
+# On average, did flights gain or lose time?
+# Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
+mean(flights$gain_in_air, na.rm = TRUE)  # 3.26134>0
+
+# Create a data.frame of flights headed to SeaTac ('SEA'), only including the
+# origin, destination, and the "gain_in_air" column you just created
+SEA <- select(flights, origin, dest, gain_in_air)
+
+# On average, did flights to SeaTac gain or loose time?
+
+# Consider flights from JFK to SEA. What was the average, min, and max air time
+# of those flights? Bonus: use pipes to answer this question in one statement
+# (without showing any other data)!
